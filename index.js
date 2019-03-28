5b884b8081e77c1c2138dc6fd85a82603c219cf8
Louise Norris <louisenorris73@yahoo.co.uk>
2019-03-28 23:36:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function theBeatlesPlay(musicians, instruments) {
   return theyPlay
 }
 
-function johnLennonFacts(){
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice", "foo", "bar"];
+function johnLennonFacts(facts){
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var newFacts = [];
   var i = 0
   while (i < facts.length) {
